4849cd97f07cc5f83a1c8d9659ee1adb440fb25c
Muhammad Ilham <93032687+Ammm24@users.noreply.github.com>
2023-11-16 19:17:33 +0700

    Update

---
+++ Web PORTOFOLIO AFRIT/UTS_AFRIT/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Portofolio</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header>
+    <h1>Portofolio</h1>
+    <nav>
+      <a href="#">Home</a>
+      <a href="#">Tentang Saya</a>
+      <a href="#">Kontak</a>
+    </nav>
+  </header>
+  <main>
+    <section class="home">
+      <h2>Home</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget ultricies quam. Morbi tincidunt, mauris ac ultricies iaculis, elit quam semper risus, ut porttitor quam eros eget enim.
+      </p>
+    </section>
+    <section class="tentang-saya">
+      <h2>Tentang Saya</h2>
+      <p>
+        Nama saya [nama], saya seorang [pekerjaan]. Saya memiliki pengalaman [jumlah] tahun di bidang [bidang]. Saya memiliki keterampilan [keterampilan].
+      </p>
+    </section>
+    <section class="kontak">
+      <h2>Kontak</h2>
+      <p>
+        Anda dapat menghubungi saya melalui [email] atau [nomor telepon].
+      </p>
+    </section>
+  </main>
+  <footer>
+    <p>Copyright &copy; 2023 [nama]</p>
+  </footer>
+</body>
+</html>
